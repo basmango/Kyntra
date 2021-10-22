e066bf1a0e93363fd42ca736a8a2ae2553db6e5c
Bassam <bassampervez@gmail.com>
2021-10-23 01:48:45 +0530

    registered models and app, fixed model issues

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from ecommerce.models import Buyer,Seller,Category,Cart,CartItem,Product,ProductImage
 # Register your models here.
+admin.site.register(Buyer)
+admin.site.register(Seller)
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+
