fce102efaae0aaabf9198d41f4d809e92328b717
shashankdaima <daimashashank10@gmail.com>
2021-11-06 19:40:46 +0530

    get All sellers Products checkpoint.

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -122,52 +122,8 @@ def admin_products(request):
 
 
 def seller_all_products(request):
-    products = [
-        {
-            "name": "Phone",
-            "price": 100,
-            "description":"This is a phone",
-            "supertag": "SOLD",
-            "image_uri": "https://media.istockphoto.com/photos/mobile-phone-top-view-with-white-screen-picture-id1161116588?k=20&m=1161116588&s=612x612&w=0&h=NKv_O5xQecCHZic53onobxjqGfW7I-D-tBrzXaPbj_Q="
-        },
-        {
-            "name": "Phone",
-            "price": 100,
-            "description":"This is a phone",
-            "supertag": "SOLD",
-            "image_uri": "https://media.istockphoto.com/photos/mobile-phone-top-view-with-white-screen-picture-id1161116588?k=20&m=1161116588&s=612x612&w=0&h=NKv_O5xQecCHZic53onobxjqGfW7I-D-tBrzXaPbj_Q="
-        },
-        
-        {
-            "name": "Phone",
-            "price": 100,
-            "description":"This is a phone",
-            "supertag": "SOLD",
-            "image_uri": "https://media.istockphoto.com/photos/mobile-phone-top-view-with-white-screen-picture-id1161116588?k=20&m=1161116588&s=612x612&w=0&h=NKv_O5xQecCHZic53onobxjqGfW7I-D-tBrzXaPbj_Q="
-        },
-        {
-            "name": "Phone",
-            "price": 100,
-            "description":"This is a phone",
-            "supertag": "SOLD",
-            "image_uri": "https://media.istockphoto.com/photos/mobile-phone-top-view-with-white-screen-picture-id1161116588?k=20&m=1161116588&s=612x612&w=0&h=NKv_O5xQecCHZic53onobxjqGfW7I-D-tBrzXaPbj_Q="
-        },
-        {
-            "name": "Phone",
-            "price": 100,
-            "description":"This is a phone",
-            "supertag": "SOLD",
-            "image_uri": "https://media.istockphoto.com/photos/mobile-phone-top-view-with-white-screen-picture-id1161116588?k=20&m=1161116588&s=612x612&w=0&h=NKv_O5xQecCHZic53onobxjqGfW7I-D-tBrzXaPbj_Q="
-        },
-        {
-            "name": "Phone",
-            "price": 100,
-            "description":"This is a phone",
-            "supertag": "SOLD",
-            "image_uri": "https://media.istockphoto.com/photos/mobile-phone-top-view-with-white-screen-picture-id1161116588?k=20&m=1161116588&s=612x612&w=0&h=NKv_O5xQecCHZic53onobxjqGfW7I-D-tBrzXaPbj_Q="
-        },
    
-    ]
+    products=Product.objects.all()
     return render(request, 'seller/all_products.html', {'name': 'seller_all_products',"products":products})
 def seller_registration(request):
     return render(request, 'seller/seller_registration.html', {'name': 'seller_registration'})
