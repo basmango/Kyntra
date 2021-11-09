6bb5b8562cfeb1b6955b70f4fea90bc0a6341415
Bassam <bassampervez@gmail.com>
2021-11-09 23:36:13 +0530

    added support for non compulsory second image

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -67,7 +67,7 @@ class Product(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     rating = models.FloatField(default=0)
     image1 = models.ImageField(upload_to='media')
-    image2 = models.ImageField(upload_to='media')
+    image2 = models.ImageField(upload_to='media',null=True)
 
     def __str__(self):
         return self.name
