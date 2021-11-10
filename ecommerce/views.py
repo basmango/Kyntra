e2c69d147aad04489f22f6cc4425bf7b9b3ee6a0
Samarth Saxena <saxena.samarth02@gmail.com>
2021-11-10 17:43:09 +0530

    Admin: Add products fixed

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -515,7 +515,7 @@ def admin_addproduct(request):
         # if this is a POST request we need to process the form data
         if request.method == 'POST':
             # create a form instance and populate it with data from the request:
-            form = AdminAddProductsForm(request.POST)
+            form = AdminAddProductsForm(request.POST,request.FILES)
             # check whether it's valid:
             if form.is_valid():
                 product = form.save()
